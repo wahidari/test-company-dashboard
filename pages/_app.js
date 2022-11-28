24dba0a5446e19d68445d88bf2a92420f0912f22
wahidari <wahiid.ari@gmail.com>
2022-11-28 11:18:22 +0700

    add next themes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
-import { GlobalProvider } from "@utils/GlobalContext";
+// import { GlobalProvider } from "@utils/GlobalContext";
 import "@styles/globals.css";
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <GlobalProvider>
+    // <GlobalProvider>
+    <ThemeProvider attribute="class" storageKey='theme'>
       <Component {...pageProps} />
-    </GlobalProvider>
+    </ThemeProvider>
+    // </GlobalProvider>
   )
 }
 
